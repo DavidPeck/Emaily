692d5eb1f664dfa810d2f0a92f2425a23051e97d
Peck <D2Px@pge.com>
2018-08-19 12:37:33 -0700

    canned mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,12 +10,24 @@ class Mailer extends helper.Mail {
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
     }
 
     formatAddresses(recipients) {
         return recipients.map(( { email } ) => {
             return new helper.Email(email);
         });
+    }
+
+    addClickTracking() {
+        const trackingSetting = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSetting.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSetting);
     }
 }
 
