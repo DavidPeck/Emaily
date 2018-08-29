c95f950af960a33a35d12963e1a9552f74d68033
David Peck <d2px@pge.com>
2018-08-28 17:06:39 -0700

    completed clients side surveyForm

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    {label: 'Campaign Title', name: 'title', error: 'Please give the survey a title!'},
+    {label: 'Subject Line', name: 'subject'},
+    {label: 'Email Body', name: 'body'},
+    {label: 'Recipient List', name: 'recipients'}
+];
